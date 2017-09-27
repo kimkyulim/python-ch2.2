c9876ad02f50246f137e8cfa8ea2ba639e382844
kyulikim0605@gmail.com <kyulikim0605@gmail.com>
2017-09-27 15:45:40 +0900

    Initial commit

---
+++ int.py
@@ -0,0 +1,16 @@
+
+a = 23
+print(type(a))
+print(isinstance(a, int))
+print(isinstance(a, bool))
+
+
+c = 0o23
+
+
+# 3.x에서는 int 와 long이 합쳐졌다.
+# 따라서 표현 범위가 굉장히 크다.
+
+# 변환 함수
+print(oct(38))
+print(hex(38))
